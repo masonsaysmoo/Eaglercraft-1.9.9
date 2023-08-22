9fc51f94cd9be98ca3f0eebd31281109840cbc2a
masonsaysmoo <96794750+masonsaysmoo@users.noreply.github.com>
2023-08-22 10:54:41 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,8 +7,8 @@ setserver="true"
 syncweb="true"
 syncjars="true"
 #server name and motd -- DO NOT USE BACKTICKS (`) within it!! -- stuff WONT work if you DO!! ALSO do not use "${anything}" UNLESS YOU KNOW WHAT YOU ARE DOING!!
-srvname="Your Minecraft Server"
-srvmotd="Minecraft Server"
+srvname="Hypixel"
+srvmotd="That one server"
 
 
 #DANGER!! setting the following to "true" will redownload the bukkit server! only change if you know what you are doing!
